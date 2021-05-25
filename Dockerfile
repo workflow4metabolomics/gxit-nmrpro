c34e22e41056f1f178fbb97a47cac801befa91fe
Lain Pavot <lain.pavot@inrae.fr>
2021-05-25 10:35:03 +0200

    feat(readers:zip files): add support for zip files

--- Dockerfile
+++ Dockerfile
@@ -10,7 +10,8 @@ ENV LANG en_US.UTF-8
 # Install NMRPro
 RUN pip install django==1.9.7
 RUN pip install numpy scipy patterns url
-RUN pip install nmrpro
+# RUN pip install nmrpro
+RUN pip install git+https://github.com/ahmohamed/nmrpro@e2e54e3999106739e88dca371687dfa78d26200e
 RUN pip install django-nmrpro
 RUN django-admin startproject nmrpro_server
 
@@ -21,5 +22,9 @@ RUN python /nmrpro_server/manage.py migrate
 
 COPY resources/django_nmrpro/encoder.py /usr/local/lib/python2.7/site-packages/django_nmrpro/encoder.py
 COPY resources/django_nmrpro/views.py   /usr/local/lib/python2.7/site-packages/django_nmrpro/views.py
+COPY resources/NMRFileManager.py        /usr/local/lib/python2.7/site-packages/nmrpro/
+COPY resources/readers.py               /usr/local/lib/python2.7/site-packages/nmrpro/
+COPY resources/constants.py             /usr/local/lib/python2.7/site-packages/nmrpro/
+COPY resources/classes/test_classes.py  /usr/local/lib/python2.7/site-packages/nmrpro/classes/
 
 CMD python /nmrpro_server/manage.py runserver 0.0.0.0:8000
---
+++ resources/readers.py
@@ -0,0 +1,83 @@
+from nmrglue.fileio import bruker, pipe
+from nmrglue.fileio.fileiobase import unit_conversion, uc_from_udic
+from .utils import make_uc_pipe
+from .NMRFileManager import get_files, find_pdata
+from classes.NMRSpectrum import NMRSpectrum
+import nmrglue.process.pipe_proc as pp
+from nmrglue import __version__ as ng_version
+from .exceptions import NoNMRDataError
+
+from .constants import BRUKER_FORMAT
+from .constants import AUTODETECT_FORMAT
+from .constants import PIPE_FORMAT
+from .constants import SPARKY_FORMAT
+
+ng_version = float(ng_version[:3])
+
+def fromFile(file, format=AUTODETECT_FORMAT):
+    if format == AUTODETECT_FORMAT:
+        files = get_files(file)
+        
+        if files is None:
+            raise NoNMRDataError('The path supplied has no NMR spectra: %s' %file)
+        elif len(files) == 1:
+            return fromFile(*files[0])
+        else:
+            results = []
+            for file in files:
+                try:
+                    results.append(fromFile(*file))
+                except IOError:
+                    pass
+            return results
+    if format == BRUKER_FORMAT:
+        return fromBruker(file)
+    if format == PIPE_FORMAT:
+        return fromPipe(file)
+    raise ValueError("Unknown format: {}".format(format))
+
+def fromBruker(file, remove_filter=True, read_pdata=True):
+    dic, data = bruker.read(file);
+    if read_pdata:
+        pdata_file = find_pdata(file, data.ndim)
+        
+        if(pdata_file is not None):
+            procs, data = bruker.read_pdata(pdata_file)
+        else: read_pdata = False
+    
+    if remove_filter and not read_pdata:
+        if ng_version > 0.5:
+            data = bruker.remove_digital_filter(dic, data, True)
+        else:
+            data = bruker.remove_digital_filter(dic, data)
+    
+    u = bruker.guess_udic(dic, data)    
+    u["original_format"] = 'Bruker'
+    u["Name"] = str(file)
+    if(read_pdata):
+        for i in range(0, data.ndim):
+            u[i]['complex'] = False
+            u[i]['freq'] = True
+            u[i]['time'] = False
+    
+    uc  = [uc_from_udic(u, dim) for dim in range(0, data.ndim)]
+    
+    if read_pdata and 'procs' in procs.keys():
+        proc_names = ['procs', 'proc2s']
+        for i, uc_i in enumerate(uc[::-1]):
+            if procs[proc_names[i] ] and procs[proc_names[i] ]['OFFSET']:
+                uc_i._first = procs[proc_names[i] ]['OFFSET']
+    
+    return NMRSpectrum(data, udic=u, uc=uc)
+
+def fromPipe(file):
+    dic, data = pipe.read(file)
+    if dic['FDTRANSPOSED'] == 1.:
+        dic, data = pp.tp(dic, data, auto=True)
+
+    u = pipe.guess_udic(dic, data)
+    u["original_format"] = 'Pipe'
+    u["Name"] = str(file)
+    
+    uc = [make_uc_pipe(dic, data, dim) for dim in range(0, data.ndim)]
+    return NMRSpectrum(data, u, uc=uc)
